6a33c6bd8eaae126884370cbf576b96ff8f7ad1a
Dmytro Zheltko <dimas105105@gmail.com>
2026-08-11 23:05:06 +0300

    9 js hw, using vite

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+function saveToLocalStorage() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFromLocalStorage() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  saveToLocalStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+});
+
+loadFromLocalStorage();
